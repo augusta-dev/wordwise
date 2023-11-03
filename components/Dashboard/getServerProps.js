9c791559db3c1b01f76024d921135c89d8127e85
augustan-dev <augustan.dev@gmail.com>
2023-11-03 17:26:46 +0300

    update

---
+++ components/Dashboard/getServerProps.js
@@ -0,0 +1,57 @@
+import { parseTemplate } from "url-template";
+const getWord = require("tdk-all-api");
+const error = "error fetching";
+let key = process.env.NEXT_PUBLIC_YANDEX_KEY;
+//key name must begin with "NEXT_PUBLIC"
+const getServerSideProps = async (word, language) => {
+	const wordd = JSON.stringify(word);
+	let dil = "en-tr";
+	language !== "English" ? (dil = "tr-en") : null;
+
+	const path = parseTemplate(
+		`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=${dil}&text=${word}`,
+	);
+	const pathUrl = path.expand({
+		key: key,
+		word: wordd,
+	});
+	const pathEng = parseTemplate(
+		`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+	);
+	const pathEngUrl = pathEng.expand({
+		word: wordd,
+	});
+
+	let [result, def] = [];
+	if (language === "English") {
+		try {
+			let [res, definition] = await Promise.all([
+				fetch(pathUrl),
+				fetch(pathEngUrl),
+			]);
+			[result, def] = await Promise.all([res.json(), definition.json()]);
+			def = def[0];
+			return [result, def];
+		} catch (err) {
+			console.log(error);
+			return [error];
+		}
+	} else {
+		try {
+			let [res, definition] = await Promise.all([
+				fetch(pathUrl),
+				getWord(word),
+			]);
+			[result, def] = await Promise.all([res.json(), definition]);
+			return [result, def];
+		} catch (err) {
+			console.error(err);
+			return [error];
+		}
+		// console.log(res, definition);
+
+		// console.log(result, def);
+		// return ([result, def]);
+	}
+};
+export default getServerSideProps;
