d0ac2d2c9a4bc6f85b09439a9caa03fb22bad4c3
augustan-dev <augustan.dev@gmail.com>
2024-02-20 01:09:47 +0300

    update

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,42 +5,42 @@ export default function Layout(props) {
 	const isDesktop = props.isDesktop;
 	return (
 		<div className="flex flex-col flex-wrap">
-			<div className={`${mobile && 'w-80 h-96'} ${isDesktop && 'w-fit h-fit'} fixed`}>
+			<div className={`${mobile && 'w-72  h-72'} ${isDesktop && 'w-fit h-fit'} fixed`}>
 				<div
 					className={`${mobile && "w-36 h-36 blur-[60px] "} ${
-						isDesktop && "w-96 h-96 blur-[140px]"
+						isDesktop && "w-72 h-72 blur-[140px]"
 					} left-0 top-0 fixed`}
 				>
 					<div
 						className={`${mobile && "w-36 h-36 -left-8 -top-8 "} ${
-							isDesktop && "w-96 h-96 -ml-28 -mt-12"
+							isDesktop && "w-72 h-72 -ml-28 -mt-12"
 						} absolute z-10 bg-blurBlue bg-opacity-100 rounded-full`}
 					/>
 				</div>
 				<div
 					className={`${
-						mobile && "w-32 h-32 -top-10 -left-40 blur-[80px] "
+						mobile && "w-32 h-32 -top-10 -left-40 blur-[72px] "
 					} ${
 						isDesktop &&
-						"w-80 h-80 -top-20 left-[500px] blur-[150px]"
+						"w-72 h-72 -top-20 left-[500px] blur-[150px]"
 					} -ml-0 float-right`}
 				>
 					<div
 						className={`${
 							mobile && "w-32 h-32 -left-40 top-10 -ml-10 "
 						} ${
-							isDesktop && "w-80 h-80 -mt-24 ml-[240px]"
+							isDesktop && "w-72 h-72 -mt-24 ml-[240px]"
 						} z-0 bg-red bg-opacity-90 rounded-full `}
 					/>
 				</div>
 				<div
 					className={`${mobile && "w-36 h-36 blur-[50px]"} ${
-						isDesktop && "w-80 h-80 blur-[150px]"
+						isDesktop && "w-72 h-72 blur-[150px]"
 					} relative "`}
 				>
 					<div
 						className={`${mobile && "w-36 h-36 -left-10 top-24"} ${
-							isDesktop && "w-80 h-80 -ml-24 top-40"
+							isDesktop && "w-72 h-72 -ml-24 top-40"
 						} absolute bg-purple-400 bg-opacity-90 rounded-full`}
 					/>
 				</div>
@@ -48,42 +48,42 @@ export default function Layout(props) {
 			<div
 				className={`${
 					mobile &&
-					"w-80 h-96 top-[calc(100vh-220px)] left-[calc(100vw-240px)]"
+					"w-72 h-72 top-[calc(100vh-220px)] left-[calc(100vw-240px)]"
 				} ${
 					isDesktop &&
-					"w-fit h-fit top-[calc(100vh-540px)] left-[calc(100vw-450px)]"
+					"w-fit h-fit top-[calc(100vh-490px)] left-[calc(100vw-430px)]"
 				} fixed`}
 			>
 				<div
 					className={`${mobile && "w-36 h-36 blur-[100px]"} ${
-						isDesktop && "w-80 h-80 blur-[170px]"
+						isDesktop && "w-72 h-72 blur-[170px]"
 					} relative`}
 				>
 					<div
-						className={`${mobile && "w-36 h-36 left-20"} ${
-							isDesktop && "w-80 h-80 left-56"
-						} top-6 absolute bg-purple-400 bg-opacity-90 rounded-full`}
+						className={`${mobile && "w-36 h-36 left-20 top-6"} ${
+							isDesktop && "w-72 h-72 left-40 top-3"
+						}  absolute bg-purple-400 bg-opacity-90 rounded-full`}
 					/>
 				</div>
 				<div
 					className={`${mobile && "w-32 h-32 -top-10 blur-[100px]"} ${
-						isDesktop && "w-80 h-80 -ml-40 mt-10 blur-[170px]"
+						isDesktop && "w-72 h-72 -ml-32 -mt-4 blur-[170px]"
 					} float-left`}
 				>
 					<div
 						className={`${mobile && "w-32 h-32"} ${
-							isDesktop && "w-80 h-80"
+							isDesktop && "w-72 h-72"
 						} z-0 bg-red bg-opacity-90 rounded-full`}
 					/>
 				</div>
 				<div
 					className={`${mobile && "w-36 h-36 blur-[100px]"} ${
-						isDesktop && "w-96 h-96 blur-[150px]"
-					}  left-0 top-0`}
+						isDesktop && "w-72 h-72 blur-[150px] ml-8"
+					}  left-0 `}
 				>
 					<div
 						className={`${mobile && "w-36 h-36"} ${
-							isDesktop && "w-96 h-96"
+							isDesktop && "w-72 h-72"
 						} left-32 -top-8 absolute z-10 bg-blurBlue bg-opacity-100 rounded-full`}
 					/>
 				</div>
