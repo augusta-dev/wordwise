d61e155088825f2e7a665203258bbcab5c1ede1e
augustan-dev <augustan.dev@gmail.com>
2024-02-21 02:37:16 +0300

    update

--- app/Providers.js
+++ app/Providers.js
@@ -1,12 +1,15 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import React, { ReactNode } from "react";
-import SizeProvider from '../components/responsive/SizeProvider';
+import SizeProvider from "../components/responsive/SizeProvider";
+import Responsive from "../components/responsive/Responsive";
 
 function Providers(props) {
 	return (
 		<SessionProvider>
-			<SizeProvider>{props.children}</SizeProvider>
+			<SizeProvider>
+				<Responsive>{props.children}</Responsive>
+			</SizeProvider>
 		</SessionProvider>
 	);
 }
