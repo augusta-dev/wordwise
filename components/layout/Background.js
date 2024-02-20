26eb815f13a9b9d4fc9e50f84750d0742ef329ba
augustan-dev <augustan.dev@gmail.com>
2024-02-21 02:53:39 +0300

    update

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -1,29 +1,29 @@
 "use client";
-import { useContext, useEffect, useState } from "react";
-import Layout from "./Layout";
 import { useMediaQuery } from "react-responsive";
-// import Responsive from "../responsive/Responsive";
-
+import { useState, useEffect } from "react";
+import Layout from "./Layout";
 const Background = (props) => {
 	const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
-	const isTablet = useMediaQuery({
-		query: `(max-width: 1024px; min-width: 768px)`,
-	});
+	const [mobile, setMobile] = useState(false);
+
+	const isTablet = useMediaQuery({ query: `(max-width: 1024px)` });
 	const [isDesktop, setIsDesktop] = useState(false);
 	useEffect(() => {
 		!isMobile && !isTablet ? setIsDesktop(true) : setIsDesktop(false);
 	}, [isMobile, isTablet]);
 
+	useEffect(() => {
+		isMobile ? setMobile(true) : setMobile(false);
+	}, [isMobile]);
+
 	return (
 		<>
-			{/* <Responsive> */}
 			<Layout
-				mobile={isMobile}
+				mobile={mobile}
 				isDesktop={isDesktop}
 			>
 				{props.children}
 			</Layout>
-			{/* </Responsive> */}
 		</>
 	);
 };
