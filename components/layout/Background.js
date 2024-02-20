94fa2bcc7aae58f92ab8abc5d82c5f627754e1a4
augustan-dev <augustan.dev@gmail.com>
2024-02-21 02:48:35 +0300

    update

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -1,37 +1,25 @@
 "use client";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Layout from "./Layout";
 import { useMediaQuery } from "react-responsive";
 // import Responsive from "../responsive/Responsive";
-import SizeContext from "../responsive/SizeContext";
 
 const Background = (props) => {
-	const sizeCtx = useContext(SizeContext);
 	const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
 	const isTablet = useMediaQuery({
 		query: `(max-width: 1024px; min-width: 768px)`,
 	});
-
+	const [isDesktop, setIsDesktop] = useState(false);
 	useEffect(() => {
-		!isMobile && !isTablet
-			? sizeCtx.setDesktop(true)
-			: sizeCtx.setDesktop(false);
+		!isMobile && !isTablet ? setIsDesktop(true) : setIsDesktop(false);
 	}, [isMobile, isTablet]);
-
-	useEffect(() => {
-		isMobile ? sizeCtx.setMobile(true) : sizeCtx.setMobile(false);
-	}, [isMobile]);
-
-	useEffect(() => {
-		isTablet ? sizeCtx.setTablet(true) : sizeCtx.setTablet(false);
-	}, [isTablet]);
 
 	return (
 		<>
 			{/* <Responsive> */}
 			<Layout
 				mobile={isMobile}
-				isDesktop={sizeCtx.desktop}
+				isDesktop={isDesktop}
 			>
 				{props.children}
 			</Layout>
