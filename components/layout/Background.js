027b983c807a817323998799c0cca8071453a801
augustan-dev <augustan.dev@gmail.com>
2024-02-21 03:01:19 +0300

    update

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -5,9 +5,14 @@ import Layout from "./Layout";
 const Background = (props) => {
 	const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
 	const [mobile, setMobile] = useState(false);
+	const [isTablet, setTablet] = useState(false)
 
-	const isTablet = useMediaQuery({ query: `(max-width: 1024px; min-width: 768px)` });
+	const isBelowLaptop = useMediaQuery({ query: `(max-width: 1024px)` });
+	const isAboveMobile = useMediaQuery({ query: `(min-width: 768px)` });
 	const [isDesktop, setIsDesktop] = useState(false);
+	useEffect(()=> {
+		(isBelowLaptop && isAboveMobile) ? setTablet(true) : setTablet(false);
+	}, [isBelowLaptop, isAboveMobile])
 	useEffect(() => {
 		!isMobile && !isTablet ? setIsDesktop(true) : setIsDesktop(false);
 	}, [isMobile, isTablet]);
