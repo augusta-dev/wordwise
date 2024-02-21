b36a3e5706b67a5ebddda623bc0e2ef366d894a7
augustan-dev <augustan.dev@gmail.com>
2024-02-21 03:45:59 +0300

    update

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -1,40 +1,27 @@
 "use client";
-import { useMediaQuery } from "react-responsive";
-import { useState, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Layout from "./Layout";
-import Responsive from '../responsive/Responsive';
+import { useMediaQuery } from "react-responsive";
+import Responsive from "../responsive/Responsive";
+import SizeContext from "../responsive/SizeContext";
+
 const Background = (props) => {
-	const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
-	
-	const [isTablet, setTablet] = useState(false)
-
-	const isBelowLaptop = useMediaQuery({ query: `(max-width: 1024px)` });
-	const isAboveMobile = useMediaQuery({ query: `(min-width: 768px)` });
-
-	const [isDesktop, setIsDesktop] = useState(false);
-	const [mobile, setMobile] = useState(false);
-
-	useEffect(()=> {
-		(isBelowLaptop && isAboveMobile) ? setTablet(true) : setTablet(false);
-	}, [isBelowLaptop, isAboveMobile])
-	
-	useEffect(() => {
-		!isMobile && !isTablet ? setIsDesktop(true) : setIsDesktop(false);
-	}, [isMobile, isTablet]);
-
-	useEffect(() => {
-		isMobile ? setMobile(true) : setMobile(false);
-	}, [isMobile]);
+	const sizeCtx = useContext(SizeContext);
+	const mobile = sizeCtx.mobile;
+	const tablet = sizeCtx.tablet;
+	const desktop = sizeCtx.desktop;
 
 	return (
-		<Responsive>
+		<>
+			<Responsive>
 			<Layout
 				mobile={mobile}
-				isDesktop={isDesktop}
+				isDesktop={desktop}
 			>
 				{props.children}
 			</Layout>
-		</Responsive>
+			</Responsive>
+		</>
 	);
 };
 export default Background;
